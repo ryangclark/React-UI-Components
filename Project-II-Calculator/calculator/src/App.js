45a013e91b3b306be1fe833785ed0732403d8f5f
Clark <rclark010@gmail.com>
2019-02-12 15:48:37 -0700

    MVP achieved. No functionality.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButtonContainer from './components/ButtonComponents/NumberButtonContainer';
+
 const App = () => {
   return (
-    Initial
+    <div className="calculator-wrapper">
+      <CalculatorDisplay />
+      <NumberButtonContainer />
+      <ActionButton id="clear" buttonValue="clear" />
+      <ActionButton id="add" buttonValue="+" />
+      <ActionButton id="divide" buttonValue="÷" />
+      <ActionButton id="equal" buttonValue="=" />
+      <ActionButton id="multiply" buttonValue="×" />
+      <ActionButton id="subtract" buttonValue="–" />
+    </div>
   );
 };
 
